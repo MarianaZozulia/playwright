d756d7ae6ef53b8a2226798a2ac5b2c1cf224c78
“Mariana <workmaryana@gmail.com>
2024-07-11 23:53:24 +0300

    added the fixture and storage state

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -4,15 +4,12 @@ const GaragePage = require('../integrations/pageObjects/pages/garagePage');
 const test = base.extend({
   
   fixturedGaragePage: async ({ browser }, use) => {
-    // Завантажуємо збережений стан сесії
+
     const context = await browser.newContext({ storageState: 'storageState.json' });
     const page = await context.newPage();
     await page.goto(`${process.env.BASE_URL_STAGING}panel/garage`);
     const garagePage = new GaragePage(page);
-    //garagePage.goto();
-    // Передаємо об'єкт сторінки у фікстуру
     await use(garagePage);
-    // Закриваємо контекст після використання
     await context.close();
   },
 });
