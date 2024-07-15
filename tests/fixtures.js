aa0b134444b94768de55958a9a3028fc82535697
“Mariana <workmaryana@gmail.com>
2024-07-15 23:11:05 +0300

    changed the setup

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -5,9 +5,9 @@ const test = base.extend({
   
   fixturedGaragePage: async ({ browser }, use) => {
 
-    const context = await browser.newContext({ storageState: 'storageState.json' });
+    const context = await browser.newContext({ storageState: process.env.AUTH_FILE_PATH });
     const page = await context.newPage();
-    await page.goto(`${process.env.BASE_URL_STAGING}panel/garage`);
+    await page.goto(`${process.env.BASE_URL_STAGING}/panel/garage`);
     const garagePage = new GaragePage(page);
     await use(garagePage);
     await context.close();
